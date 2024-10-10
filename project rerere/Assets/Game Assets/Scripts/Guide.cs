3ffb4ba22e55862938e6c37596c77a6d4ec6f19d
ashtyyyxxxz <147919956+ashtyyyxxxz@users.noreply.github.com>
2024-10-10 20:56:56 +0300

    guide and npc

--- project rerere/Assets/Game Assets/Scripts/Guide.cs
+++ project rerere/Assets/Game Assets/Scripts/Guide.cs
@@ -1,26 +1,60 @@
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.InputSystem;
 
 public class Guide : MonoBehaviour
 {
     [SerializeField] private Transform[] pointsOfInterest;
-
-    [SerializeField] private Animator animator;
+    [SerializeField] private AudioClip[] speech;
+    [SerializeField] private Animator guideAnimator;
+    [SerializeField] private Animation buttonAnimations;
+    [SerializeField] private InputActionReference aButton;
 
     private NavMeshAgent agent;
+    private AudioSource audioSource;
 
     private int currentPoint = 0;
+    private bool inZone;
+
+    private void OnEnable()
+    {
+        aButton.action.performed += AButton;
+    }
+
+    private void AButton(InputAction.CallbackContext obj)
+    {
+        if(inZone)
+        {
+            GoToNextPoint();
+        }
+    }
+
+    private void OnDisable()
+    {
+        aButton.action.performed -= AButton;
+    }
 
     private void Awake()
     {
         agent = GetComponent<NavMeshAgent>();
 
         WalkToPoint();
+        audioSource.clip = speech[currentPoint];
+        audioSource.Play();
+    }
+
+    private void Update()
+    {
+        if (Vector3.Distance(transform.position, pointsOfInterest[currentPoint].position) <= 0.5f)
+        {
+            guideAnimator.SetBool("IsWalking", false);
+        }
     }
 
     private void WalkToPoint()
     {
         agent.SetDestination(pointsOfInterest[currentPoint].position);
+        guideAnimator.SetBool("IsWalking", true);
     }
 
     public void GoToNextPoint()
@@ -31,17 +65,26 @@ public class Guide : MonoBehaviour
             currentPoint--;
             return;
         }
+        audioSource.clip = speech[currentPoint];
+        audioSource.Play();
         WalkToPoint();
     }
 
-    public void GoToPreviousPoint()
+    private void OnTriggerEnter(Collider other)
     {
-        currentPoint--;
-        if (currentPoint < 0)
+        if (other.CompareTag("Player"))
         {
-            currentPoint++;
-            return;
+            inZone = true;
+            buttonAnimations.Play("InteractButtonAppear");
         }
-        WalkToPoint();
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            inZone = false;
+            buttonAnimations.Play("InteractButtonDisappear");
+        }
     }
 }
--- project rerere/Assets/Game Assets/Scripts/RegularNPC.cs
+++ project rerere/Assets/Game Assets/Scripts/RegularNPC.cs
@@ -1,30 +1,56 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class RegularNPC : MonoBehaviour
 {
     private NavMeshAgent agent;
+    private Animator animator;
+
+    private Vector3 goalPos;
+
+    private bool isReadyToWalk = true;
 
     private void Awake()
     {
         agent = GetComponent<NavMeshAgent>();
+        animator = GetComponentInChildren<Animator>();
     }
 
-    private void Start()
+    private void Update()
     {
-        InvokeRepeating(nameof(WalkToRandomPoint), 2, 10);
+        if (isReadyToWalk)
+        {
+            goalPos = GetRandomPosition();
+
+            WalkToPoint(goalPos);
+        }
+
+        if(Vector3.Distance(transform.position,goalPos) <= 0.5f)
+        {
+            animator.SetBool("IsWalking", false);
+        }
     }
 
-    private void WalkToRandomPoint() 
+    private void WalkToPoint(Vector3 position) 
     { 
         agent.SetDestination(GetRandomPosition());
+        animator.SetBool("IsWalking", true);
+        StartCoroutine(WalkCooldown());
+    }
+
+    private IEnumerator WalkCooldown()
+    {
+        isReadyToWalk = false;
+        yield return new WaitForSeconds(14);
+        isReadyToWalk = true;
     }
 
     private Vector3 GetRandomPosition()
     {
         Vector3 position = new Vector3(Random.Range(-100f, 100f), 0, Random.Range(-100f, 100f));
         NavMeshHit hit;
-        NavMesh.SamplePosition(position, out hit, 10, 1);
+        NavMesh.SamplePosition(position, out hit, 100, 1);
         position = hit.position;
 
         return position;
